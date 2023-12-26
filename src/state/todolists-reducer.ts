4055053e060b6e95c128f448175c757846ae4fe0
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2023-12-26 22:05:16 +0300

    added some tests

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,3 +1,4 @@
+import { v1 } from "uuid";
 import { TodolistType } from "../App";
 
 type ActionType = {
@@ -12,6 +13,31 @@ export const todolistsReducer = (
   switch (action.type) {
     case "REMOVE-TODOLIST": {
       return state.filter((tl) => tl.id !== action.id);
+    }
+
+    case "ADD-TODOLIST": {
+      let todolist: TodolistType = {
+        id: v1(),
+        filter: "all",
+        title: action.title,
+      };
+      return (state = [...state, todolist]);
+    }
+
+    case "CHANGE-TODOLIST-TITLE": {
+      let todolist = state.find((tl) => tl.id === action.id);
+      if (todolist) {
+        todolist.title = action.title;
+      }
+      return (state = [...state]);
+    }
+
+    case "CHANGE-TODOLIST-FILTER": {
+      let todolist = state.find((tl) => tl.id === action.id);
+      if (todolist) {
+        todolist.filter = action.filter;
+      }
+      return (state = [...state]);
     }
 
     default:
