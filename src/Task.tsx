675743a2a8b2ebc522eec166127c9fc7ea31934c
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-02-15 22:40:30 +0300

    fix - starting refactor Task

--- src/Task.tsx
+++ src/Task.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { EditableSpan } from "./EditableSpan";
 
 export const Task = () => {
-  return <div>Task</div>;
+  const removeTask = () => props.removeTask(t.id, props.id);
+  const onChangeCheckHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    props.changeStatus(t.id, e.currentTarget.checked, props.id);
+  };
+  const onChangeTitleHandler = (newValue: string) => {
+    props.changeTitle(t.id, newValue, props.id);
+  };
+
+  return (
+    <div>
+      (
+      <li key={t.id} className={t.isDone ? "is-done" : ""}>
+        <input
+          type="checkbox"
+          checked={t.isDone}
+          onChange={onChangeCheckHandler}
+        />
+        <EditableSpan title={t.title} onChange={onChangeTitleHandler} />
+        <button onClick={removeTask}>x</button>
+      </li>
+      )
+    </div>
+  );
 };
