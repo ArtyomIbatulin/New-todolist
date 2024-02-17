3ca1790d38a6039eec4057df7c93aad53d699fb3
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-02-17 12:51:39 +0300

    fixed refactored Task comp

--- src/Task.tsx
+++ src/Task.tsx
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { ChangeEvent, FC } from "react";
 import { EditableSpan } from "./EditableSpan";
+import { TaskType } from "./Todolist";
 
-export const Task = () => {
-  const removeTask = () => props.removeTask(t.id, props.id);
+export type TaskPropsType = {
+  task: TaskType;
+  todolistId: string;
+  removeTask: (id: string, todolistId: string) => void;
+  changeStatus: (taskId: string, isDone: boolean, todolistId: string) => void;
+  changeTitle: (taskId: string, newValue: string, todolistId: string) => void;
+};
+
+export const Task: FC<TaskPropsType> = React.memo((props) => {
+  const removeTask = () => props.removeTask(props.task.id, props.todolistId);
   const onChangeCheckHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    props.changeStatus(t.id, e.currentTarget.checked, props.id);
+    props.changeStatus(
+      props.task.id,
+      e.currentTarget.checked,
+      props.todolistId
+    );
   };
   const onChangeTitleHandler = (newValue: string) => {
-    props.changeTitle(t.id, newValue, props.id);
+    props.changeTitle(props.task.id, newValue, props.todolistId);
   };
 
   return (
-    <div>
-      (
-      <li key={t.id} className={t.isDone ? "is-done" : ""}>
-        <input
-          type="checkbox"
-          checked={t.isDone}
-          onChange={onChangeCheckHandler}
-        />
-        <EditableSpan title={t.title} onChange={onChangeTitleHandler} />
-        <button onClick={removeTask}>x</button>
-      </li>
-      )
-    </div>
+    <li key={props.task.id} className={props.task.isDone ? "is-done" : ""}>
+      <input
+        type="checkbox"
+        checked={props.task.isDone}
+        onChange={onChangeCheckHandler}
+      />
+      <EditableSpan title={props.task.title} onChange={onChangeTitleHandler} />
+      <button onClick={removeTask}>x</button>
+    </li>
   );
-};
+});
